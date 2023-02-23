9a6ba7e9a81928ba5ca0a0e8d0b407c8ebd3009c
Jayden Serenari <jserenari@pitt.edu>
2023-02-23 17:06:35 -0500

    Added command line argument to enable GUI mode

--- src/main/java/me/jmser/jbas/JBas.java
+++ src/main/java/me/jmser/jbas/JBas.java
@@ -1,11 +1,16 @@
 package me.jmser.jbas;
 
-import me.jmser.jbas.interfaces.GUI;
-import me.jmser.jbas.interfaces.JBasicInterface;
+import me.jmser.jbas.interfaces.*;
 
 public class JBas {
     public static void main(String[] args){
-        JBasicInterface iface = new GUI();
+        JBasicInterface iface;
+
+        if(args.length > 0 && (args[0].equals("-g") || args[0].equals("--gui"))){
+            iface = new GUI();
+        } else {
+            iface = new CLI();
+        }
 
         iface.run();
     }
